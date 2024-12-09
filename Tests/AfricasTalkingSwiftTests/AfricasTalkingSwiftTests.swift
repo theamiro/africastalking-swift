b120ae8bc4412dec6f49d59c4750b54a3855b5de
Michael Amiro <odindo.michael@gmail.com>
2024-12-09 08:49:39 +0300

    Fix tests replace Swift Testing with XCTests

--- Tests/AfricasTalkingSwiftTests/AfricasTalkingSwiftTests.swift
+++ Tests/AfricasTalkingSwiftTests/AfricasTalkingSwiftTests.swift
@@ -1,6 +1,6 @@
-import Testing
+import XCTest
 @testable import AfricasTalkingSwift
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class AfricasTalkingSwiftTests: XCTestCase {
+    
 }
